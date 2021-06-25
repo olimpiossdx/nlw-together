e4e3d0ef46880f6fbf4b21b236c44ea5ddbfc815
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-06-25 15:35:54 -0300

    Rename

--- letmeask/src/hooks/useRoom/useRoom.tsx
+++ letmeask/src/hooks/useRoom/useRoom.tsx
@@ -52,7 +52,7 @@ const useRoom = (roomId: string) => {
         }
       });
 
-      setTitle(databaseRoom.title);
+      setTitle(databaseRoom.titulo);
       setQuestions(parsedQuestions);
     });
 
